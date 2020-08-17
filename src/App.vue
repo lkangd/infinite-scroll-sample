5c5043d7a9be09032bb3c64ea64008997da4fb0a
CurtisLiong <lkangd@gmail.com>
2020-08-17 19:14:56 +0800

    refactor: 删除多余文件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-size: 0;
+  margin: 0;
+  padding: 0;
+  line-height: 1;
+  box-sizing: border-box;
 }
 </style>
