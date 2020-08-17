ac71f1d519be2ac2c8ced886fb1b83a6d1ea8cdf
CurtisLiong <lkangd@gmail.com>
2020-08-17 19:16:02 +0800

    feat: 添加导航页,路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Router from 'vue-router';
 
-Vue.config.productionTip = false
+import navs from './navs';
+import heightDynamic from './heightDynamic';
+import heightFixed from './heightFixed';
+
+Vue.use(Router);
+Vue.config.productionTip = false;
 
 new Vue({
+  router: new Router({
+    routes: [
+      {
+        path: '/',
+        component: navs,
+      },
+      {
+        path: '/height-dynamic',
+        component: heightDynamic,
+      },
+      {
+        path: '/height-fixed',
+        component: heightFixed,
+      },
+    ],
+  }),
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/navs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navs">
+    <router-link to="/height-fixed">height-fixed</router-link>
+    <router-link to="/height-dynamic">height-dynamic</router-link>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+
+export default {
+  name: 'navs',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.navs {
+  padding: 20px;
+  a {
+    display: block;
+    margin-bottom: 10px;
+    font-size: 20px;
+  }
+}
+</style>
